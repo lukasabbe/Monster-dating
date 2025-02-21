8b516f0dd31760e033ed1dd5b0991b9dfead9ea9
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-02-22 00:32:24 +0100

    aaa

--- Assets/Scripts/MonsterDialogue.cs
+++ Assets/Scripts/MonsterDialogue.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using DialogueSystem;
+using NUnit.Framework;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MonsterDialogue", menuName = "Scriptable Objects/MonsterDialogue")]
@@ -10,4 +11,6 @@ public class MonsterDialogue : ScriptableObject
     public float base_rep;
     public List<FoodItemScriptableObj> req_food_items = new();
     public List<string> food_comments = new();
+    public List<float> end_succes_values = new(3);
+    public List<string> end_comments = new(3);
 }
